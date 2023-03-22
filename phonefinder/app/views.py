37bdcd3491feae101005d93a5dda8e43ba7bc50c
fcooper222 <2685295c@student.gla.ac.uk>
2023-03-22 00:18:40 +0000

    bare bones phone search added

--- phonefinder/app/views.py
+++ phonefinder/app/views.py
@@ -1,12 +1,17 @@
 from django.core.exceptions import ValidationError
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
+from sympy import re
 from app.forms import UserForm, UserProfileForm
 from app.models import Review, Favourite
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.password_validation import validate_password
 from django.urls import reverse
+import json
+from urllib.parse import urlencode
+import os
+from app.filter import filterPhones
 
 
 def index(request):
@@ -125,28 +130,78 @@ def about(request):
 
 
 def database(request):
-    return render(request, 'app/database.html')
+    if request.method == "POST":
+        phone_json = json.loads(request.body)
+        phones = filterPhones(phone_json)
+        print(phones)
 
-
-def submit_form(request):
-    if request.method == 'POST':
-        # process the form data as needed
-        data = {
-            'brand': request.POST.get('brand'),
-            'min_year': int(request.POST.get('min_year')),
-            'max_year': int(request.POST.get('max_year')),
-        }
-        # create a JSON response with the desired data
-        response_data = {'success': True, 'data': data}
-        return JsonResponse(response_data)
+        # now we filter phones and render the database page
+        context = {'phones': phones}
+        return JsonResponse(context, safe=False)
     else:
-        # return an error response if the request method is not POST
-        response_data = {'success': False, 'error': 'Invalid request method'}
-        return JsonResponse(response_data, status=400)
+        return render(request, 'app/database.html')
+
+
+# def submit_form(request):
+#     if request.method == 'POST':
+#         # process the form data as needed
+#         data = {
+#             'brand': request.POST.get('brand'),
+#             'min_year': int(request.POST.get('min_year')),
+#             'max_year': int(request.POST.get('max_year')),
+#         }
+#         # create a JSON response with the desired data
+#         response_data = {'success': True, 'data': data}
+#         return JsonResponse(response_data)
+#     else:
+#         # return an error response if the request method is not POST
+#         response_data = {'success': False, 'error': 'Invalid request method'}
+#         return JsonResponse(response_data, status=400)
 
 
 def find(request):
-    return render(request, 'app/find.html')
+
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    json_file_path = os.path.join(BASE_DIR, 'phones.json')
+    with open(json_file_path) as json_file:
+        phones = json.load(json_file)
+
+    # Create an empty dictionary to hold the possible values for each attribute
+        brands = []
+        widths = []
+        heights = []
+        storages = []
+        rams = []
+
+        # Loop through each phone and add its attributes to the corresponding list if they're unique
+        for phone in phones:
+            if phone['brand'] not in brands:
+                brands.append(phone['brand'])
+            for resolution in phone['resolution'].split(','):
+                width, height = resolution.split('x')
+                if int(width) not in widths:
+                    widths.append(int(width))
+                if int(height) not in heights:
+                    heights.append(int(height))
+            storage = int(float(phone['storage']))
+            if storage not in storages:
+                storages.append(storage)
+            ram = int(float(phone['ram']))
+            if ram not in rams:
+                rams.append(ram)
+
+        # Construct the context dictionary with the lists of unique attribute values
+        context_dict = {
+            'brands': brands,
+            'widths': sorted(widths),
+            'heights': sorted(heights),
+            'storages': sorted(storages),
+            'rams': sorted(rams),
+        }
+        json_file.close()
+
+# Render the template with the context dictionary
+    return render(request, 'app/find.html', context_dict)
 
 
 @login_required
